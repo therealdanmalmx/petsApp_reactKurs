7aa124bee4a78804775739eff2a511a15fd86329
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2023-02-08 09:12:40 +0100

    Refactor comonent names. Initial styling.

--- src/PetItems.js
+++ src/PetItems.js
@@ -1,7 +1,13 @@
-const PetItems = () => {
+import './App.css';
+
+const PetItems = ({pet}) => {
+    const { name, species, favFoods, birthYear, photo } = pet;
+
     return (
-        <div>
-            <h1>This is Pet Items</h1>
+        <div className="pets">
+            <div className='pets-card'>
+                {name}
+            </div> 
         </div>
     )
 };
--- src/Pets.js
+++ src/Pets.js
@@ -1,11 +1,26 @@
+import { useEffect, useState } from 'react';
+import './App.css';
 import PetItems from './PetItems'
-const Pets  = () => {
-    return (
-        <div>
-            <h1>This is pets</h1>
-            <h2>And <PetItems /></h2>
-        </div>
-    )
+
+function Pets() {
+  const [data, setData] = useState(undefined);
+  
+  useEffect(() => {
+    const petsList = require('./pets.json');
+    setData(petsList);
+  }, [])
+    
+  console.log({data})
+  return (
+    <div className="App">
+      <h1 className="pets-title">Them my pets ya'll</h1>
+      <div className="pets-flex">
+        {data.pets.map((pet) => (
+          <PetItems pet={pet} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Pets;
