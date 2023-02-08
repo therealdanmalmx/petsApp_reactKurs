43dd74c0ca2fe0d6e0eee9f1b79df1a660a1876b
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2023-02-08 15:51:02 +0100

    Refactoring. Add remove method (WIP)

--- src/PetItems.js
+++ src/PetItems.js
@@ -3,19 +3,23 @@ import './App.css';
 const PetItems = ({pet}) => {
     const { name, species, favFoods, birthYear, photo } = pet;
 
+    const removeMe = (name) => {
+        console.log('I was removed', name)
+    }
     return (
         <div className="pets">
-            <div className='pets-card'>
-                { photo ? (
+            <div className='pets-card' key={name}>
+                { photo && (
                     <div>
                         <img className="pets-image"src={photo} alt="my pet"/>
                         <p className="pets-info">{name ? <strong>{name}'s</strong> : null} my name. {species ? species : null} is my game</p>
+                        {birthYear && (<p>I was born in {birthYear}</p>)}
                         {favFoods && (
-                        <p>And I just love: {favFoods.map((food) => `${food}, `)}</p>
+                        <p className="pets-food">And I just love: {favFoods.map((food) => `${food}, `)}</p>
                         )}
-                        <p>I was born in{birthYear ? birthYear : null} making me {Date.now() - birthYear} years old</p>
+                        <button type="button" className="pets-button" onClick={() => removeMe(name)}>Don't like me?</button>
                     </div>
-                ) : null }
+                )}
             </div> 
         </div>
     )
