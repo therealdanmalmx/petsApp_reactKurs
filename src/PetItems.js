55b05f0d2dd431e96bc2994acaeb1b737e5f5a7f
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2023-02-09 09:16:51 +0100

    Add remove pet function - WIP

--- src/PetItems.js
+++ src/PetItems.js
@@ -1,23 +1,21 @@
 import './App.css';
 
-const PetItems = ({pet}) => {
+const PetItems = ({pet, removePet}) => {
+    
     const { name, species, favFoods, birthYear, photo } = pet;
 
-    const removeMe = (name) => {
-        console.log('I was removed', name)
-    }
     return (
         <div className="pets">
-            <div className='pets-card' key={name}>
+            <div className='pets-card'>
                 { photo && (
                     <div>
                         <img className="pets-image"src={photo} alt="my pet"/>
-                        <p className="pets-info">{name ? <strong>{name}'s</strong> : null} my name. {species ? species : null} is my game</p>
+                        <p className="pets-info">{name && <strong>{name}'s</strong>} my name. {species && species} is my game</p>
                         {birthYear && (<p>I was born in {birthYear}</p>)}
                         {favFoods && (
                         <p className="pets-food">And I just love: {favFoods.map((food) => `${food}, `)}</p>
                         )}
-                        <button type="button" className="pets-button" onClick={() => removeMe(name)}>Don't like me?</button>
+                        <button type="button" className="pets-button" onClick={() => removePet(name)}>Don't like me?</button>
                     </div>
                 )}
             </div> 
--- src/Pets.js
+++ src/Pets.js
@@ -3,12 +3,18 @@ import './App.css';
 import PetItems from './PetItems'
 
 function Pets() {
-  const [data, setData] = useState(undefined);
+  const [data, setData] = useState([]);
+
+  const removePet = (name) => {
+    console.log(name)
+    const newList = data.pets.filter((pet) => pet.name !== name);
+    setData(newList);
+  }
   
   useEffect(() => {
     const petsList = require('./pets.json');
     setData(petsList);
-  }, [])
+  }, [setData])
     
   console.log('data', data)
   return (
@@ -16,7 +22,7 @@ function Pets() {
       <h1 className="pets-title">Them my pets ya'll</h1>
       <div className="pets-flex">
         {data.pets.map((pet) => (
-          <PetItems pet={pet} />
+          <PetItems key={pet.name} pet={pet} removePet={removePet}/>
         ))}
       </div>
     </div>
