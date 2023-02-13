f495cf950b5baa8482b0b4600bc2e477e5de6180
therealdanmalmx <danmalmx@gmail.com>
2023-02-13 22:40:32 +0100

    Adding removePet prop and function

--- src/PetItems.js
+++ src/PetItems.js
@@ -1,26 +1,38 @@
-import './App.css';
+import "./App.css";
 
-const PetItems = ({pet, removePet}) => {
-    
-    const { name, species, favFoods, birthYear, photo } = pet;
+const PetItems = ({ pet, removePet }) => {
+  const { name, species, favFoods, birthYear, photo } = pet;
 
-    return (
-        <div className="pets">
-            <div className='pets-card'>
-                { photo && (
-                    <div>
-                        <img className="pets-image"src={photo} alt="my pet"/>
-                        <p className="pets-info">{name && <strong>{name}'s</strong>} my name. {species && species} is my game</p>
-                        {birthYear && (<p>I was born in {birthYear}</p>)}
-                        {favFoods && (
-                        <p className="pets-food">And I just love: {favFoods.map((food) => `${food}, `)}</p>
-                        )}
-                        <button type="button" className="pets-button" onClick={() => removePet(name)}>Don't like me?</button>
-                    </div>
-                )}
-            </div> 
-        </div>
-    )
+  //   const deleteThings = (name) => removePet(name);
+
+  return (
+    <div className="pets">
+      <div className="pets-card">
+        {photo && (
+          <div>
+            <img className="pets-image" src={photo} alt="my pet" />
+            <p className="pets-info">
+              {name && <strong>{name}'s</strong>} my name. {species && species}{" "}
+              is my game
+            </p>
+            {birthYear && <p>I was born in {birthYear}</p>}
+            {favFoods && (
+              <p className="pets-food">
+                And I just love: {favFoods.map((food) => `${food}, `)}
+              </p>
+            )}
+            <button
+              type="button"
+              className="pets-button"
+              onClick={() => removePet(name)}
+            >
+              Don't like me?
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default PetItems;
--- src/Pets.js
+++ src/Pets.js
@@ -1,29 +1,30 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import PetItems from './PetItems'
+import { useState, useEffect } from "react";
+import "./App.css";
+import PetItems from "./PetItems";
+const petsList = require("./pets.json");
 
 function Pets() {
   const [data, setData] = useState([]);
 
-  const removePet = (name) => {
-    console.log(name)
-    const newList = data.pets.filter((pet) => pet.name !== name);
-    setData(newList);
-  }
-  
   useEffect(() => {
-    const petsList = require('./pets.json');
     setData(petsList);
-  }, [setData])
-    
-  console.log('data', data)
+  }, [data]);
+
+  const removePet = (name) => {
+    let newList = data.pets.filter((pet) => pet.name !== name);
+    console.log({ newList });
+    setData(newList);
+  };
+
+  console.log("data", data);
   return (
     <div className="App">
       <h1 className="pets-title">Them my pets ya'll</h1>
       <div className="pets-flex">
-        {data.pets.map((pet) => (
-          <PetItems key={pet.name} pet={pet} removePet={removePet}/>
-        ))}
+        {data.pets.length &&
+          data.pets.map((pet) => (
+            <PetItems key={pet.name} pet={pet} removePet={removePet} />
+          ))}
       </div>
     </div>
   );
